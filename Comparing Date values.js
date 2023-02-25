caf17a00183876ff0a53eff2645232efcb11c22d
husain3000 <73273019+husain3000@users.noreply.github.com>
2023-02-25 15:27:40 +0530

    first commit

---
+++ Comparing Date values.js
@@ -0,0 +1,11 @@
+// var date1 = new Date();
+// var date2 = new Date(date1.valueOf() + 10);
+// console.log(date1.valueOf() === date2.valueOf());
+
+// var date1 = new Date();
+// var date2 = new Date();
+// console.log(date1 === date2);
+
+var date1 = new Date();
+var date2 = date1;
+console.log(date1 === date2);
